112e745c609b93b6cf5752a76bddaa74f3210972
minakarimi1 <minakarimi1@yahoo.com>
2023-10-24 20:36:00 +0330

    create home-route.js

---
+++ routes/home-route.js
@@ -0,0 +1,7 @@
+import express from "express";
+import homeController from "../controller/home-controller.js";
+const router = express.Router();
+
+router.get('/',homeController.getHomeController);
+
+export default router;
