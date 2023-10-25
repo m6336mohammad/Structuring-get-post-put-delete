d5110efd0b842b96298c5d08f60573da19f44dca
minakarimi1 <minakarimi1@yahoo.com>
2023-10-25 22:03:18 +0330

    added getall and getId

--- controller/course-control.js
+++ controller/course-control.js
@@ -3,3 +3,17 @@ const courses = [
   { id: 2, name: "css" },
   { id: 3, name: "javascript" },
 ];
+
+//get
+const getCourseById = (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) {
+    return res.status(404).json("coures with given id not found");
+  }
+  res.send(course);
+};
+
+
+const getCourse = (req ,res)=>{
+  res.send(courses);
+};
