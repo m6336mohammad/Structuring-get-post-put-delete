73039b5a5da9c3ec0507bdabce872e906fa9d0e1
minakarimi1 <minakarimi1@yahoo.com>
2023-10-26 16:35:51 +0330

    added post course controller

--- controller/course-control.js
+++ controller/course-control.js
@@ -18,6 +18,22 @@ const getCourseById = (req, res) => {
   res.send(course);
 };
 
+//post
+const postCourse = (req,res)=>{
+  if(! req.body.name || req.body.name > 3){
+    return res.status(404).json('Name is mandatory and must be more than 3 characters');
+  };
+  const course ={
+    id: courses.length + 1,
+    name: req.body.name,
+  }
+  courses.push(course);
+  res.status(200).json(course);
+}
 
 
-export default {getCourse,getCourseById}
+export default {
+  getCourse,
+  getCourseById,
+  postCourse,
+}
