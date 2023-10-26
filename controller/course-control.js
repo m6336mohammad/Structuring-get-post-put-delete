d6a587cd6689981259993250c8111ac307b0167b
minakarimi1 <minakarimi1@yahoo.com>
2023-10-26 16:52:54 +0330

    added try/catch to post

--- controller/course-control.js
+++ controller/course-control.js
@@ -20,17 +20,27 @@ const getCourseById = (req, res) => {
 
 //post
 const postCourse = (req,res)=>{
-  if(! req.body.name || req.body.name > 3){
-    return res.status(404).json('Name is mandatory and must be more than 3 characters');
-  };
-  const course ={
-    id: courses.length + 1,
-    name: req.body.name,
-  }
-  courses.push(course);
-  res.status(200).json(course);
-}
 
+  try{ 
+     
+    if(!req.body.name || req.body.name.length < 3){
+      return res.status(404).json('Name is mandatory and must be more than 3 characters');
+    };
+    const course ={
+      id: courses.length + 1,
+      name: req.body.name,
+    }
+    courses.push(course);
+    res.status(200).json({data:course , message: 'course created successfully'});
+  
+  }catch(error){
+    console.error(error.message);
+    res.status(400).json({
+      data: null,
+      message: "Error while creating user",
+    });
+  }
+} 
 
 export default {
   getCourse,
