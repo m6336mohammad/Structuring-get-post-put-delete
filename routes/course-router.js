fbc9cff0c6fe0eb970b293f2612bd92a1e381bef
mohammad <m6336.programer@gmail.com>
2023-10-27 14:18:19 +0330

    added put route

--- routes/course-router.js
+++ routes/course-router.js
@@ -6,7 +6,7 @@ import courseControler from "./../controller/course-control.js";
 router.get("/", courseControler.getCourse);
 router.get("/:id", courseControler.getCourseById);
 router.post("/", courseControler.postCourse);
-// router.put("/:id", courseControler.updateCourse);
+router.put("/:id", courseControler.putCourse);
 // router.delete("/:id");
 
 export default router;
