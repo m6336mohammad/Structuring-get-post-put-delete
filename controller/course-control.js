37a546915a242d24f4bf0342119d297bfb5e3883
minakarimi1 <minakarimi1@yahoo.com>
2023-10-27 20:50:31 +0330

    added try /catch from deleteCourse

--- controller/course-control.js
+++ controller/course-control.js
@@ -1,15 +1,15 @@
 const courses = [
-  { id: 1, name: "html" ,page: 20},
-  { id: 2, name: "css" ,page:25},
-  { id: 3, name: "javascript",page:35 },
+  { id: 1, name: "html", page: 20 },
+  { id: 2, name: "css", page: 25 },
+  { id: 3, name: "javascript", page: 35 },
 ];
 
 //get courses
-const getCourse = (req ,res)=>{
+const getCourse = (req, res) => {
   res.send(courses);
 };
 
-//get courses by ID 
+//get courses by ID
 const getCourseById = (req, res) => {
   const course = courses.find((c) => c.id === parseInt(req.params.id));
   if (!course) {
@@ -19,29 +19,29 @@ const getCourseById = (req, res) => {
 };
 
 //post
-const postCourse = (req,res)=>{
-
-  try{ 
-     
-    if(!req.body.name || req.body.name.length < 3){
-      return res.status(404).json('Name is mandatory and must be more than 3 characters');
-    };
-    const course ={
+const postCourse = (req, res) => {
+  try {
+    if (!req.body.name || req.body.name.length < 3) {
+      return res
+        .status(404)
+        .json("Name is mandatory and must be more than 3 characters");
+    }
+    const course = {
       id: courses.length + 1,
       name: req.body.name,
-    }
+    };
     courses.push(course);
-    res.status(200).json({data:course , message: 'course created successfully'});
-  
-  }catch(error){
+    res
+      .status(200)
+      .json({ data: course, message: "course created successfully" });
+  } catch (error) {
     console.error(error.message);
     res.status(400).json({
       data: null,
       message: "Error while creating user",
     });
   }
-} 
-
+};
 
 const putCourse = (req, res) => {
   const courseId = parseInt(req.params.id);
@@ -55,20 +55,27 @@ const putCourse = (req, res) => {
   courses[courseIndex].name = user.name; // Assuming that you expect a "name" field in the request body
   courses[courseIndex].page = user.page; // Assuming that you expect a "page" field in the request body
 
-  res.status(200).json({ data: courses[courseIndex], code: 200, message: "OK" });
+  res
+    .status(200)
+    .json({ data: courses[courseIndex], code: 200, message: "OK" });
 };
 
-const deleteCourseById = (req,res)=>{
- 
-  const course = courses.find((c)=> c.id === parseInt(req.params.id));
-  if(!course){return res.status(404).send('coures with given id not found')};
+const deleteCourseById = (req, res) => {
+  try {
+    const course = courses.find((c) => c.id === parseInt(req.params.id));
+    if (!course) {
+      return res.status(404).send("coures with given id not found");
+    }
+
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
+    res.send({ data: course, message: "deleted successful" });
   
-  const index = courses.indexOf(course);
-  courses.splice(index,1);
-  res.send({data:course , message: "deleted successful"});
-
-
-}
+  } catch (error) {
+    console.log(error.message);
+    res.status(400).send({ data: null, message: "Error while delete course" });
+  }
+};
 
 export default {
   getCourse,
@@ -76,4 +83,4 @@ export default {
   postCourse,
   putCourse,
   deleteCourseById,
-}
+};
